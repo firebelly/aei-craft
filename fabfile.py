3e91ef26099f9d9d83ed2f9c099ff9cd1e8eda54
Nate Beaty <nate@clixel.com>
2021-01-07 12:31:36 -0500

    switch to fabric 2 for deploy, keep archived fabric 1 fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,53 +1,35 @@
-from fabric.api import *
-import os
+from fabric import task, Connection
+from invoke import run as local
 
-env.hosts = ['aei-craft.firebelly.co']
-env.user = 'firebelly'
-env.path = '~/Sites/aei-craft'
-env.remotepath = '/home/firebelly/apps/aei'
-env.git_branch = 'master'
-env.warn_only = True
-env.forward_agent = True
+remote_path = "/home/aeieng/apps/aei_craft_staging"
+remote_hosts = ["aeieng@aeieng.opalstacked.com"]
+php_command = "php74"
 
-def production():
-  env.hosts = ['aeieng.opalstacked.com']
-  env.user = 'aeieng'
-  env.remotepath = '/home/aeieng/apps/aei_craft_production'
-  env.git_branch = 'master'
+# set to production
+@task
+def production(c):
+    global remote_hosts, remote_path
+    remote_hosts = ["aeieng@aeieng.com"]
+    remote_path = "/home/aeieng/apps/aei_craft_production"
 
-def staging():
-  env.hosts = ['aeieng.opalstacked.com']
-  env.user = 'aeieng'
-  env.remotepath = '/home/aeieng/apps/aei_craft_staging'
-  env.git_branch = 'master'
+# deploy
+@task(hosts=remote_hosts)
+def deploy(c):
+    update(c)
+    composer_update(c)
+    clear_cache(c)
 
-def assets():
-  local('npx gulp --production')
+def update(c):
+    c.run("cd {} && git pull".format(remote_path))
 
-def devsetup():
-  print "Installing composer, node and bower assets...\n"
-  local('composer install')
-  local('npm install')
-  local('cd web/assets && bower install')
-  local('npx gulp')
-  local('cp .env.example .env')
-  print "OK DONE! Hello? Are you still awake?\nEdit your .env file with local credentials\nRun `npx gulp watch` to run local gulp to compile & watch assets"
+def composer_update(c):
+    c.run("cd {} && {} ~/bin/composer.phar install".format(remote_path, php_command))
 
-def deploy(composer='y'):
-  update()
-  if composer == 'y':
-    composer_install()
-  clear_cache()
+def clear_cache(c):
+    c.run("cd {} && ./craft clear-caches/compiled-templates".format(remote_path))
+    c.run("cd {} && ./craft clear-caches/data".format(remote_path))
 
-def update():
-  with cd(env.remotepath):
-    run('git pull origin {0}'.format(env.git_branch))
-
-def composer_install():
-  with cd(env.remotepath):
-    run('php74 ~/bin/composer.phar install')
-
-def clear_cache():
-  with cd(env.remotepath):
-    run('./craft clear-caches/compiled-templates')
-    run('./craft clear-caches/data')
+# local commands
+@task
+def assets(c):
+    local("npx gulp --production")
